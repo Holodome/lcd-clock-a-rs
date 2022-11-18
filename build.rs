d94e8a21f2e72d34245dc30ff1f3cdc1b8e59652
Holodome <ilal.vinogradov@gmail.com>
2022-11-18 23:17:16 +0300

    reduce directory nesting for images

--- build.rs
+++ build.rs
@@ -4,7 +4,8 @@ use walkdir::WalkDir;
 
 fn main() {
     let target_dir = PathBuf::from("target/img/");
-    for entry in WalkDir::new("misc/img").into_iter().filter_map(|e| e.ok()) {
+    let src_dir = "misc/img";
+    for entry in WalkDir::new(src_dir).into_iter().filter_map(|e| e.ok()) {
         let metadata = entry.metadata().unwrap();
         if !metadata.is_file() {
             continue;
@@ -16,9 +17,12 @@ fn main() {
 
             let img_raw = image.into_raw();
             let dim_raw = [dim.0.to_le_bytes(), dim.1.to_le_bytes()].concat();
+
+            let path = path.strip_prefix(src_dir).unwrap();
             let mut target_name = target_dir.join(path);
             target_name.set_extension("bin");
             std::fs::create_dir_all(&target_name.parent().unwrap()).ok();
+
             let mut file = File::create(target_name).unwrap();
             file.write_all(&dim_raw).unwrap();
             file.write_all(&img_raw).unwrap();
@@ -26,6 +30,4 @@ fn main() {
             println!("cargo:rerun-if-changed={}", path.to_str().unwrap());
         }
     }
-
-    assert!(false);
 }
